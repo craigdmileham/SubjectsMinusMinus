96d1809273c5c260be099ba037a0302e9d51b3fa
Joseph Pignataro <pignatarojoseph@yahoo.com>
2016-04-04 11:40:20 -0400

    Revert "Revert "test2""
    This reverts commit 9d371cee51b75e8defd472c9ec69e82dd2f36a81.

--- tests/acceptance/FeaturedDescriptionCept.php
+++
@@ -1,7 +0,0 @@
-<?php
-//barebones test of featured resource
-$I = new AcceptanceTester($scenario);
-$I->wantTo('make sure that featured resource description is visible');
-$I->amOnPage('/subjects/index.php');
-$I->see("Links to Mathematics journals, bibliographies, listservs, government resources, career information, and other general resources.");
-?>
